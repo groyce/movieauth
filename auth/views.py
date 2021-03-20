c99b58b287d0cfaee4ca35f9857dd60c2987ebf4
groyce <groyce@unomaha.edu>
2021-03-20 17:01:35 -0500

    first

---
+++ auth/views.py
@@ -0,0 +1,11 @@
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+from rest_framework import generics
+from django.contrib.auth.models import User
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
